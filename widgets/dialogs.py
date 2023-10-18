003d8a0ababfec64178672c683b2c60814958b3e
Gvozdev Leonid <gvozdev-leonid-95@yandex.ru>
2023-10-18 15:00:14 +0200

    added input wrapper

--- widgets/dialogs.py
+++ widgets/dialogs.py
@@ -3,7 +3,8 @@ from PySide6 import(
     QtGui,
     QtWidgets
 )
-from PySide_Widgets import NamedHObjectLayout
+from PySide_Widgets import NamedVObjectLayout
+from widgets.elements import ImputWrapper
 
 tr = QtCore.QCoreApplication.translate
 label_font = QtGui.QFont('Open Sans', 16, 400)
@@ -12,16 +13,27 @@ class UtilityDialog(QtWidgets.QDialog):
     def __init__(self, electricity: float = 0, water: float = 0, tax: float = 0) -> None:
         super().__init__()
 
+        self.setObjectName('Window')
+
         layout = QtWidgets.QVBoxLayout()
         layout.setContentsMargins(30, 30, 30, 30)
         layout.setSpacing(20)
 
-        electricity_field = QtWidgets.QDoubleSpinBox(minimum=0, maximum=999999999, value=electricity)
-        water_field = QtWidgets.QDoubleSpinBox(minimum=0, maximum=999999999, value=water)
-        tax_field = QtWidgets.QDoubleSpinBox(minimum=0, maximum=999999999, value=tax)
+        self.electricity_field = QtWidgets.QDoubleSpinBox(minimum=0, maximum=999999999, value=electricity)
+        self.electricity_field.setObjectName('Input')
 
-        layout.addLayout(NamedHObjectLayout(tr('UtilityDialog - Electricity', 'Electricity'), electricity_field, 'left', font=label_font))
-        layout.addLayout(NamedHObjectLayout(tr('UtilityDialog - Water', 'Water'), water_field, 'left', font=label_font))
-        layout.addLayout(NamedHObjectLayout(tr('UtilityDialog - Tax', 'Tax'), tax_field, 'left', font=label_font))
+        self.water_field = QtWidgets.QDoubleSpinBox(minimum=0, maximum=999999999, value=water)
+        self.water_field.setObjectName('Input')
+
+        self.tax_field = QtWidgets.QDoubleSpinBox(minimum=0, maximum=999999999, value=tax)
+        self.tax_field.setObjectName('Input')
+
+        button_accept = QtWidgets.QPushButton('Ok')
+        button_accept.setObjectName('DialogButton')
+
+        layout.addWidget(ImputWrapper(tr('UtilityDialog - Electricity', 'Electricity'), self.electricity_field))
+        layout.addWidget(ImputWrapper(tr('UtilityDialog - Water', 'Water'), self.water_field))
+        layout.addWidget(ImputWrapper(tr('UtilityDialog - Tax', 'Tax'), self.tax_field))
+        layout.addWidget(button_accept)
 
         self.setLayout(layout)
