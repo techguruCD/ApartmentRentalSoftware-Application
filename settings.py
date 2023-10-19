831e7b20a29572e485d28e7e1c1fc7138b80a77e
Gvozdev Leonid <gvozdev-leonid-95@yandex.ru>
2023-10-19 12:04:10 +0200

    added new colors

--- settings.py
+++ settings.py
@@ -2,5 +2,9 @@ colors = {
     'table_color_1': '#FFFFFF',
     'table_color_2': '#AAC1E2',
     'text_color_1': '#000000',
-    'text_color_2': '#FFFFFF'
+    'text_color_2': '#FFFFFF',
+
+    'primary_light': '#97B2DF',
+    'primary_normal': '#6498D4',
+    'primary_dark': '#597FAE',   
 }
