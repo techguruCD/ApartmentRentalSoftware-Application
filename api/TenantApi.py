8a886f693874e0cd7d419fcb7a40160e2483b78d
techguruCD <serhiirohachov1@gmail.com>
2023-10-19 16:57:04 +0300

    added TenantApi

---
+++ api/TenantApi.py
@@ -0,0 +1,10 @@
+def tenant_list(search: str = None, final_url: str = None) -> tuple[bool, dict | None]:
+    return True, {
+        'next': None,
+        'previous': None,
+        'results': [{'id': 0, 'status': 'active', 'phone': '123456789', 'last_name': 'last_name 1', 'first_name': 'first_name 1'},
+                    {'id': 0, 'status': 'inactive', 'phone': '987654321', 'last_name': 'last_name 2', 'first_name': 'first_name 2'}]
+    }
+
+def tenant_update(data: list) -> bool:
+    return False
--- pages/TenantPage.py
+++ pages/TenantPage.py
@@ -12,11 +12,13 @@ from PySide6.QtWidgets import (
     QFrame,
     QLineEdit,
     QPlainTextEdit,
+    QGridLayout,
     QVBoxLayout,
     QHBoxLayout,
     QPushButton,
     QSpacerItem,
-    QSizePolicy
+    QSizePolicy,
+    QLabel
 )
 
 from widgets.elements import (
@@ -33,36 +35,9 @@ class TenantPage(QFrame):
     def __init_UI(self):
         self.setObjectName("Window")
 
-        layout = QVBoxLayout()
+        layout = QGridLayout()
         layout.setContentsMargins(30, 30, 30, 30)
         layout.setSpacing(20)
-
-        self.name = QLineEdit(self)
-        self.name.setObjectName('Input')
-        self.last_name = QLineEdit(self)
-        self.last_name.setObjectName('Input')
-        name_layout = QHBoxLayout()
-        name_layout.addWidget(InputWrapper(tr('Widgets - First Name', 'First Name'), self.name))
-        name_layout.addWidget(InputWrapper(tr('Widgets - Last Name', 'Last Name'), self.last_name))
-
-        self.phone = QLineEdit(self)
-        self.phone.setObjectName('Input')
-        self.mail = QLineEdit(self)
-        self.mail.setObjectName('Input')
-        contact_layout = QHBoxLayout()
-        contact_layout.addWidget(InputWrapper(tr('Widgets - Phone', 'Phone'), self.phone))
-        contact_layout.addWidget(InputWrapper(tr('Widgets - Mail', 'Mail'), self.mail))
-
-        self.parent_address = QLineEdit(self)
-        self.parent_address.setObjectName('Input')
-        self.parent_phone = QLineEdit(self)
-        self.parent_phone.setObjectName('Input')
-        parent_contact_layout = QHBoxLayout()
-        parent_contact_layout.addWidget(InputWrapper(tr("Widgets - Parents' address", "Parents' address"), self.parent_address))
-        parent_contact_layout.addWidget(InputWrapper(tr("Widgets - Parents' phone", "Parents' phone"), self.parent_phone))
-
-        self.note = QPlainTextEdit(self)
-        self.note.setObjectName('Input')
 
         self.button_back = QPushButton(icon=QIcon('data/arrow-long-left.svg'), parent=self)
         self.button_back.setObjectName('IconButton')
@@ -71,16 +46,52 @@ class TenantPage(QFrame):
         back_layout.addWidget(self.button_back)
         back_layout.addSpacerItem(QSpacerItem(1, 1, QSizePolicy.Expanding, QSizePolicy.Minimum))
 
-        self.button_save = QPushButton(tr('Widgets - Save', 'Save'), self)
-        control_layout = QHBoxLayout()
-        control_layout.addWidget(self.button_save)
-        control_layout.addSpacerItem(QSpacerItem(1, 1, QSizePolicy.Expanding, QSizePolicy.Minimum))
+        self.label_title = QLabel(tr('TenantPage - Title', 'Tenant'), self)
+        self.label_title.setObjectName('TitleLabel')
 
-        layout.addLayout(back_layout)
-        layout.addLayout(name_layout)
-        layout.addLayout(contact_layout)
-        layout.addLayout(parent_contact_layout)
-        layout.addWidget(InputWrapper(tr('Widgets - Note', 'Note'), self.note))
-        layout.addLayout(control_layout)
+        self.first_name = QLineEdit(self)
+        self.first_name.setObjectName('Input')
+        self.last_name = QLineEdit(self)
+        self.last_name.setObjectName('Input')
+        
+        self.phone = QLineEdit(self)
+        self.phone.setObjectName('Input')
+        self.mail = QLineEdit(self)
+        self.mail.setObjectName('Input')
+        
+        self.parent_address = QLineEdit(self)
+        self.parent_address.setObjectName('Input')
+        self.parent_phone = QLineEdit(self)
+        self.parent_phone.setObjectName('Input')
+        
+        self.note = QPlainTextEdit(self)
+        self.note.setObjectName('Input')
+
+        self.button_save = QPushButton(tr('Buttons - Save', 'Save'), self)
+        self.button_save.setObjectName('DialogButton')
+        self.button_cancel = QPushButton(tr('Buttons - Cancel', 'Cancel'), self)
+        self.button_cancel.setObjectName('DialogButton')
+        
+        layout.addLayout(back_layout, 0, 0, 1, 2)
+        layout.addWidget(self.label_title, 1, 0, 1, 2)
+        layout.addWidget(InputWrapper(tr('Widgets - First Name', 'First Name'), self.first_name), 2, 0, 1, 1)
+        layout.addWidget(InputWrapper(tr('Widgets - Last Name', 'Last Name'), self.last_name), 2, 1, 1, 1)
+        layout.addWidget(InputWrapper(tr('Widgets - Phone', 'Phone'), self.phone), 3, 0, 1, 1)
+        layout.addWidget(InputWrapper(tr('Widgets - Mail', 'Mail'), self.mail), 3, 1, 1, 1)
+
+        layout.addWidget(InputWrapper(tr("Widgets - Parents' address", "Parents' address"), self.parent_address), 4, 0, 1, 1)
+        layout.addWidget(InputWrapper(tr("Widgets - Parents' phone", "Parents' phone"), self.parent_phone), 4, 1, 1, 1)
+
+        layout.addWidget(InputWrapper(tr('Widgets - Note', 'Note'), self.note), 5, 0, 1, 2)
+        layout.addWidget(self.button_save, 6, 0, 1, 1)
+        layout.addWidget(self.button_cancel, 6, 1, 1, 1)
+
+        # layout.setRowStretch(0, 0)
+        # layout.setRowStretch(1, 0)
+        # layout.setRowStretch(2, 0)
+        # layout.setRowStretch(3, 0)
+        # layout.setRowStretch(4, 0)
+        # layout.setRowStretch(5, 1)
+        # layout.setRowStretch(6, 9)
 
         self.setLayout(layout)
