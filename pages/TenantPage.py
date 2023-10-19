ea7fe4b98fec1bbedcedd7a1811e08e432c1ea6a
techguruCD <serhiirohachov1@gmail.com>
2023-10-19 17:07:09 +0300

    modified TenantPage by integrating moke version of api

--- pages/TenantPage.py
+++ pages/TenantPage.py
@@ -21,17 +21,18 @@ from PySide6.QtWidgets import (
     QLabel
 )
 
-from widgets.elements import (
-    InputWrapper
-)
+import api.TenantApi as api
+from widgets.elements import InputWrapper, CustomWindow
+from widgets.dialogs import Dialog
 
 tr = QCoreApplication.translate
 # Name!, Last Name!, Phone!, Mail!, Parents' Address, Parents' Phone, Note [text field]
-class TenantPage(QFrame):
-    def __init__(self, parent: QWidget | None):
-        super().__init__(parent)
+class TenantPage(CustomWindow):
+    def __init__(self):
+        super().__init__()
         self.setWindowTitle(tr('TenantPage - Title', 'Tenant'))
         self.__init_UI()
+
     def __init_UI(self):
         self.setObjectName("Window")
 
@@ -46,8 +47,8 @@ class TenantPage(QFrame):
         back_layout.addWidget(self.button_back)
         back_layout.addSpacerItem(QSpacerItem(1, 1, QSizePolicy.Expanding, QSizePolicy.Minimum))
 
-        self.label_title = QLabel(tr('TenantPage - Title', 'Tenant'), self)
-        self.label_title.setObjectName('TitleLabel')
+        # self.label_title = QLabel(tr('TenantPage - Title', 'Tenant'), self)
+        # self.label_title.setObjectName('TitleLabel')
 
         self.first_name = QLineEdit(self)
         self.first_name.setObjectName('Input')
@@ -69,29 +70,40 @@ class TenantPage(QFrame):
 
         self.button_save = QPushButton(tr('Buttons - Save', 'Save'), self)
         self.button_save.setObjectName('DialogButton')
+        self.button_save.clicked.connect(self.save)
         self.button_cancel = QPushButton(tr('Buttons - Cancel', 'Cancel'), self)
         self.button_cancel.setObjectName('DialogButton')
+        self.button_cancel.clicked.connect(self.cancel)
         
         layout.addLayout(back_layout, 0, 0, 1, 2)
-        layout.addWidget(self.label_title, 1, 0, 1, 2)
-        layout.addWidget(InputWrapper(tr('Widgets - First Name', 'First Name'), self.first_name), 2, 0, 1, 1)
-        layout.addWidget(InputWrapper(tr('Widgets - Last Name', 'Last Name'), self.last_name), 2, 1, 1, 1)
-        layout.addWidget(InputWrapper(tr('Widgets - Phone', 'Phone'), self.phone), 3, 0, 1, 1)
-        layout.addWidget(InputWrapper(tr('Widgets - Mail', 'Mail'), self.mail), 3, 1, 1, 1)
+        layout.addWidget(InputWrapper(tr('Widgets - First Name', 'First Name'), self.first_name), 1, 0, 1, 1)
+        layout.addWidget(InputWrapper(tr('Widgets - Last Name', 'Last Name'), self.last_name), 1, 1, 1, 1)
+        layout.addWidget(InputWrapper(tr('Widgets - Phone', 'Phone'), self.phone), 2, 0, 1, 1)
+        layout.addWidget(InputWrapper(tr('Widgets - Mail', 'Mail'), self.mail), 2, 1, 1, 1)
 
-        layout.addWidget(InputWrapper(tr("Widgets - Parents' address", "Parents' address"), self.parent_address), 4, 0, 1, 1)
-        layout.addWidget(InputWrapper(tr("Widgets - Parents' phone", "Parents' phone"), self.parent_phone), 4, 1, 1, 1)
+        layout.addWidget(InputWrapper(tr("Widgets - Parents' address", "Parents' address"), self.parent_address), 3, 0, 1, 1)
+        layout.addWidget(InputWrapper(tr("Widgets - Parents' phone", "Parents' phone"), self.parent_phone), 3, 1, 1, 1)
 
-        layout.addWidget(InputWrapper(tr('Widgets - Note', 'Note'), self.note), 5, 0, 1, 2)
-        layout.addWidget(self.button_save, 6, 0, 1, 1)
-        layout.addWidget(self.button_cancel, 6, 1, 1, 1)
+        layout.addWidget(InputWrapper(tr('Widgets - Note', 'Note'), self.note), 4, 0, 1, 2)
+        layout.addWidget(self.button_save, 5, 0, 1, 1)
+        layout.addWidget(self.button_cancel, 5, 1, 1, 1)
 
-        # layout.setRowStretch(0, 0)
-        # layout.setRowStretch(1, 0)
-        # layout.setRowStretch(2, 0)
-        # layout.setRowStretch(3, 0)
-        # layout.setRowStretch(4, 0)
-        # layout.setRowStretch(5, 1)
-        # layout.setRowStretch(6, 9)
+        self.widget.setLayout(layout)
 
-        self.setLayout(layout)
+    def save(self):
+        data = {
+            'first_name': self.first_name.text(),
+            'last_name': self.last_name.text(),
+            'phone': self.phone.text(),
+            'mail': self.mail.text(),
+            'parent_address': self.parent_address.text(),
+            'parent_phone': self.parent_phone.text()
+        }
+        if not api.tenant_save(data):
+            dialog = Dialog(tr('TenantPage - Error title', 'Save error'),
+                            tr('TenantPage - Error text', 'An error occurred while updating data!'),
+                            'error')
+            if dialog.is_accepted:
+                self.SignalClose.emit()
+    def cancel(self):
+        self.SignalClose.emit()
