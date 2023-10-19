a69a52c1c658d3cb4267e21a497f162436a45806
techguruCD <serhiirohachov1@gmail.com>
2023-10-19 21:37:03 +0300

    added moke version of ApartmentApi

---
+++ api/ApartmentApi.py
@@ -0,0 +1,10 @@
+def apartment_list(search: str = None, final_url: str = None) -> tuple[bool, dict | None]:
+    return True, {
+        'next': None,
+        'previous': None,
+        'results': [{'id': 0, 'owner': { 'first_name': 'first_name_1', 'last_name': 'last_name_1'}, 'address': 'address_1', 'unique_identifier': 'unique_identifier1'},
+                    {'id': 1, 'owner': { 'first_name': 'first_name_2', 'last_name': 'last_name_1'}, 'address': 'address_2', 'unique_identifier': 'unique_identifier2'}]
+    }
+
+def apartment_save(data: any) -> bool:
+    return False
