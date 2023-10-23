e49bfd21301075fcb78c3e4d72575062a53b2290
Gvozdev Leonid <gvozdev-leonid-95@yandex.ru>
2023-10-23 21:46:28 +0200

    added import of ready-made widgets

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,11 +1,11 @@
-from .ApartmentPage import ApartmentPage
-from .HomePage import HomePage
-from .LeaseContractPage import LeaseContractPage
-from .OwnerPage import OwnerPage
-from .ReminderPage import ReminderPage
-from .RentPaymentPage import RentPaymentPage
+# from .ApartmentPage import ApartmentPage
+# from .HomePage import HomePage
+# from .LeaseContractPage import LeaseContractPage
+# from .OwnerPage import OwnerPage
+# from .ReminderPage import ReminderPage
 # from .TaskPage import TaskPage
-from .TenantListPage import TenantListPage
-from .TenantPage import TenantPage
-from .TransactionPage import TransactionPage
-from .UtilityPaymentPage import UtilityPaymentPage
+# from .TenantListPage import TenantListPage
+# from .TenantPage import TenantPage
+# from .TransactionPage import TransactionPage
+from .RentPaymentsPage import RentPaymentsPage
+from .UtilityBillsPaymentsPage import UtilityBillsPaymentsPage
