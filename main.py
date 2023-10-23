365bf1173f0ecb85c63b56321956f3098619bcec
techguruCD <serhiirohachov1@gmail.com>
2023-10-23 23:23:33 +0300

    modified main.py to handle multiple pages

--- main.py
+++ main.py
@@ -5,12 +5,20 @@ from PySide6 import (
 )
 tr = QtCore.QCoreApplication.translate
 
-from pages.ReminderPage import (
-    ReminderPage
-)
+from pages.ReminderPage import ReminderPage
+from pages.TenantPage import TenantPage
+from pages.TenantListPage import TenantListPage
+
 import widgets.dialogs as dialogs
 
+pages = {
+    'tenantList': TenantListPage,
+    'tenant': TenantPage
+}
+
 class MainWindow(QtWidgets.QMainWindow):
+    pages = []
+    central = None
     def __init__(self) -> None:
         super().__init__()
                 
@@ -25,6 +33,10 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def onSignal(self, param):
         print(param)
+        self.pages.insert(self.pages.__len__(), self.centralWidget())
+        central = pages[param['window']]()
+        central.connect_control_signals(self)
+        self.setCentralWidget(central)
 
 def addFont(file_name):
     with open(file_name, 'rb') as file:
