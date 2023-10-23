cb4e50f9a49f3eb11f1b29b019dfb4eef8c5797d
techguruCD <serhiirohachov1@gmail.com>
2023-10-23 17:15:20 +0300

    seted note to be expanding in layout

--- pages/TenantPage.py
+++ pages/TenantPage.py
@@ -26,7 +26,7 @@ from widgets.elements import InputWrapper, CustomWindow
 from widgets.dialogs import Dialog
 
 tr = QCoreApplication.translate
-# Name!, Last Name!, Phone!, Mail!, Parents' Address, Parents' Phone, Note [text field]
+# Name!, Last Name!, Phone!, Email!, Parents' Address, Parents' Phone, Note [text field]
 class TenantPage(CustomWindow):
     def __init__(self):
         super().__init__()
@@ -57,8 +57,8 @@ class TenantPage(CustomWindow):
         
         self.phone = QLineEdit(self)
         self.phone.setObjectName('Input')
-        self.mail = QLineEdit(self)
-        self.mail.setObjectName('Input')
+        self.email = QLineEdit(self)
+        self.email.setObjectName('Input')
         
         self.parent_address = QLineEdit(self)
         self.parent_address.setObjectName('Input')
@@ -79,7 +79,7 @@ class TenantPage(CustomWindow):
         layout.addWidget(InputWrapper(tr('Widgets - First Name', 'First Name'), self.first_name), 1, 0, 1, 1)
         layout.addWidget(InputWrapper(tr('Widgets - Last Name', 'Last Name'), self.last_name), 1, 1, 1, 1)
         layout.addWidget(InputWrapper(tr('Widgets - Phone', 'Phone'), self.phone), 2, 0, 1, 1)
-        layout.addWidget(InputWrapper(tr('Widgets - Mail', 'Mail'), self.mail), 2, 1, 1, 1)
+        layout.addWidget(InputWrapper(tr('Widgets - Email', 'Email'), self.email), 2, 1, 1, 1)
 
         layout.addWidget(InputWrapper(tr("Widgets - Parents' address", "Parents' address"), self.parent_address), 3, 0, 1, 1)
         layout.addWidget(InputWrapper(tr("Widgets - Parents' phone", "Parents' phone"), self.parent_phone), 3, 1, 1, 1)
@@ -88,6 +88,8 @@ class TenantPage(CustomWindow):
         layout.addWidget(self.button_save, 5, 0, 1, 1)
         layout.addWidget(self.button_cancel, 5, 1, 1, 1)
 
+        layout.setRowStretch(4, 1)
+
         self.widget.setLayout(layout)
 
     def save(self):
@@ -95,12 +97,12 @@ class TenantPage(CustomWindow):
             'first_name': self.first_name.text(),
             'last_name': self.last_name.text(),
             'phone': self.phone.text(),
-            'mail': self.mail.text(),
+            'email': self.email.text(),
             'parent_address': self.parent_address.text(),
             'parent_phone': self.parent_phone.text(),
             'note': self.note.toPlainText()
         }
-        if not api.tenant_save(data):
+        if not api.create_tenant(data):
             dialog = Dialog(tr('TenantPage - Error title', 'Save error'),
                             tr('TenantPage - Error text', 'An error occurred while updating data!'),
                             'error')
